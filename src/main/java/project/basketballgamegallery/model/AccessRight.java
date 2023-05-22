9a339d08fd9039e65c79c64909dd4b82d615cbe2
Luke Wajda <83553257+mkdirer@users.noreply.github.com>
2023-05-22 08:31:44 +0200

    Add files via upload

---
+++ src/main/java/project/basketballgamegallery/model/AccessRight.java
@@ -0,0 +1,30 @@
+package project.basketballgamegallery.model;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "accessrights")
+public class AccessRight {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private EnumAccessRight name;
+    public AccessRight() {
+    }
+    public AccessRight(EnumAccessRight name) {
+        this.name = name;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public EnumAccessRight getName() {
+        return name;
+    }
+    public void setName(EnumAccessRight name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/project/basketballgamegallery/model/EnumAccessRight.java
@@ -0,0 +1,7 @@
+package project.basketballgamegallery.model;
+
+public enum EnumAccessRight {
+    USER_ACCESS_RIGHT,
+    MODERATOR_ACCESS_RIGHT,
+    ADMIN_ACCESS_RIGHT
+}
